1ced6d6d4425baf84b1dfec35208c4e36a9cccf2
Angelica Harrison <harrison_angelica@yahoo.com>
2018-08-03 13:28:28 -0400

    heroku working 2

--- db/connection.js
+++ db/connection.js
@@ -6,5 +6,5 @@ module.exports = mongoose;
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL)
 } else {
-  mongoose.connect("mongodb://localhost/whenpresident");
+  mongoose.connect("mongodb://localhost/scentall");
 }
